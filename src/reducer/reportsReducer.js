ae1d95489e0ebebd146599a6e1c45139cdbf3365
ubaideforte <ubaid@eforte.net>
2021-03-09 00:20:43 +0500

    add dispatcher

--- src/reducer/reportsReducer.js
+++ src/reducer/reportsReducer.js
@@ -1,7 +1,9 @@
-const initialValue = { Report: "Report reducer is created dynamically" };
+const initialValue = { report: "Report reducer is created dynamically" };
 
 const reportsReducer = (state = initialValue, action) => {
   switch (action.type) {
+    case "ADD_REPORT":
+      return { ...state, report: action.payload };
     default:
       return state;
   }
--- src/views/Reports.jsx
+++ src/views/Reports.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { withReducer } from "../withReducer";
 import reportsReducer from "../reducer/reportsReducer";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 const Reports = () => {
+  const report = useSelector((state) => state.reports.report);
+  const dispatch = useDispatch();
+
+  const [state, setState] = useState();
+
   return (
     <div>
       <h1>Reports</h1>
+      <h3>{`Value of report ${report}`}</h3>
+
       <Link to="/">Home</Link>
+
+      <input value={state} onChange={(e) => setState(e.target.value)} />
+      <button onClick={() => dispatch({ type: "ADD_REPORT", payload: state })}>
+        Update Report
+      </button>
     </div>
   );
 };
